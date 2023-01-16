2ecc1e5f0c03c625a61deae1473644a1a3a099c7
Xian Yang <xian.yang@extern.edeka.de>
2023-01-16 09:55:38 +0100

    notes

--- create_sp.sh
+++ create_sp.sh
@@ -5,7 +5,12 @@ groupId=$(az group show \
 az ad sp create-for-rbac \
   --scope $groupId \
   --role Contributor \
-  --sdk-auth # Attention, "--sdk-auth" is going to be deprecated.
+  --sdk-auth # Attention, "--sdk-auth" is going to be deprecated. BUT:
+  # Note: The above az ad sp create-for-rbac command will give you the --sdk-auth deprecation warning. 
+  # As we are working with CLI for this deprecation process, we strongly recommend users to use this --sdk-auth flag 
+  # as the result dictionary output changes and not accepted by login action if --sdk-auth is not used.
+
+  # Note: the scope would be too large if it was only for pulling and pushing images. It is not if the sp is going to do other jobs in the rg. 
 
 ClientId=
 
